5e0d659417fe68a2dbfbb7cf4cc6902c3bba5a94
emmaduarte <emma.ale.duarte@gmail.com>
2023-07-29 20:53:29 -0300

    add arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
-import React, {useState, useEffect} from 'react'
-import Listado from './Componentes/Listado/Listado'
-import Detalles from './Componentes/Detalles/Detalles'
-import Nav from './Componentes/Nav/Nav'
-import { BrowserRouter, Route, Routes} from "react-router-dom"
-import Busqueda from './Componentes/Busqueda/Busqueda'
+import React, { useState, useEffect } from "react";
+import Listado from "./Componentes/Listado/Listado";
+import Detalles from "./Componentes/Detalles/Detalles";
+import Nav from "./Componentes/Nav/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Busqueda from "./Componentes/Busqueda/Busqueda";
 
 const App = () => {
-  
-  
   return (
-    <>
-      
-    
-     <div>
-      
       <BrowserRouter basename="/movies">
-        <Nav/>
-          <Routes>
-            
-            <Route path='/'  element={<Listado/>}/>
-            <Route path='/Detalles/:title/:id'  element={<Detalles/>}/>
-            <Route path='/Busqueda/:query'  element={<Busqueda/>}/>
-            
-          </Routes>
-        
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Listado />} />
+          <Route path="/Detalles/:title/:id" element={<Detalles />} />
+          <Route path="/Busqueda/:query" element={<Busqueda />} />
+        </Routes>
       </BrowserRouter>
-      
-      </div>
-      
-</>
-      
-  )
-}
+  );
+};
 
-export default App
+export default App;
