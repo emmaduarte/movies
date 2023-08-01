a439fe46733e1adfe252e406a0e71b0eba77bf3c
emmaduarte <emma.ale.duarte@gmail.com>
2023-08-01 01:24:59 -0300

    fix navegacion

--- src/Componentes/Listado/Listado.jsx
+++ src/Componentes/Listado/Listado.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import './listado.css'
-
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function Listado()  {
@@ -9,6 +9,8 @@ export default function Listado()  {
   const api_url = "https://api.themoviedb.org/3"
   const api = "575bcbc20d4282f26a0fda173e2568e3"
   const [page, setPage] = useState(1)
+
+  const navigate = useNavigate();
 
   const lista = async (page) => {
     return await fetch(`${api_url}/movie/popular?api_key=${api}&language=es-ES&page=${page}`)
@@ -38,7 +40,12 @@ export default function Listado()  {
                       <div key={item.id}className="peliculas">
                         <img src={`${image_url + item.poster_path}`} alt="Imagen pelicula" className='image'/>
                         <div className='titulo'>{item.title}</div>
-                        <div><button className="boton1"><a href={`/movies/Detalles/${item.title}/${item.id}`}> Informacion</a></button></div>
+                        <div>
+                          <button className="boton1" onClick={() => navigate(`/Detalles/${item.title}/${item.id}`)}>
+                            {/*<a href={`/movies/Detalles/${item.title}/${item.id}`}> Informacion</a>*/} 
+                            Informacion
+                          </button>
+                        </div>
                       </div>))
                   }
                   
